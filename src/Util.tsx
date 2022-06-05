319add60db8e7f0257ee229bbb6c15e923a91097
Gavin Zeng <gavin8503@gmail.com>
2022-06-05 21:09:38 +0800

    Add function

--- src/Util.tsx
+++ src/Util.tsx
@@ -37,8 +37,17 @@ export const longToMonthNum = (dt:number) => {
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ]
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
 export const longToMonthStr = (dt:number) => {
     return monthNames[new Date(dt * 1000).getMonth()]
+}
+
+export const getDayStr = (dt:number) => {
+    const today = new Date().getDay();
+    const dtDay = new Date(dt * 1000).getDay();
+    return today === dtDay ? "Today" : weekday[dtDay]
 }
 
 export const longToMonthWithDate = (dt:number) => {
@@ -52,8 +61,21 @@ export const longToMonthWithDate = (dt:number) => {
     return `${monthNames[dts.getMonth()]} ${day} ${hours}:${minutes}`
 }
 
+export const longToHourMinute = (dt: number) => {
+    const dts = new Date(dt * 1000)
+    let hours:number|string = dts.getHours(),
+        minutes:number|string = dts.getMinutes()
+    if (hours < 10) hours = '0' +  hours
+    if (minutes < 10) minutes = '0' + minutes
+    return `${hours}:${minutes}`
+}
+
 export const longToGetDate = (dt:number) => {
     return new Date(dt * 1000).getDate()
+}
+
+export const longToGetTime = (dt:number) => {
+    return new Date(dt * 1000).getTime()
 }
 
 export const msToKmh = (ms:number) => {
