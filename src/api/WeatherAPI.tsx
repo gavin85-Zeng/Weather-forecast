fe8710ab6550aa2db8ad25b6c4d91a79990f9666
Gavin Zeng <gavin8503@gmail.com>
2022-06-05 20:55:17 +0800

    Change units to metric

--- src/api/WeatherAPI.tsx
+++ src/api/WeatherAPI.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import ILocation from "../model/LocationInterface"
+import { ILocationInfo } from "../model/WeatherInterface"
 import { Country } from "../Util"
 
 const ENV_API_KEY = process.env.WEATHER_API_KEY !== undefined ? process.env.WEATHER_API_KEY : ''
@@ -9,36 +9,39 @@ const GEO_PARAM = `q=?loc&limit=1&appid=?apikey`
 const GEO_REVSERSE_URL = 'geo/1.0/reverse?'
 const GEO_REVSERSE_PARAM = `lat=?lat&lon=?lon&limit=1&appid=?apikey`
 const WEAHTER_URL = 'data/2.5/onecall?'
-const WEATHER_PARAM = `lat=?lat&lon=?lon&appid=?apikey`
+const WEATHER_PARAM = `lat=?lat&lon=?lon&appid=?apikey&units=metric`
 
-export const getCountryInfo = async (loc:string, key?:string):Promise<ILocation> => {
-    let obj:ILocation = {
-        lat: undefined,
-        lon: undefined,
-        city: undefined,
-        countryName: undefined
-    }
-    let KEY = key === undefined ? ENV_API_KEY : key
+export const getCountryInfo = async (loc:string, key?:string): Promise<ILocationInfo> => {
+    const KEY = key !== undefined && key !== '' ?  key : ENV_API_KEY;
     const url = `${API_URL}${GEO_URL}${GEO_PARAM.replace('?loc', loc).replace('?apikey', KEY)}`
     
+    const obj: ILocationInfo = {
+        lat: "",
+        lon: "",
+        name: "",
+        countryName: ""
+    }
+
     await axios.get(url)
-        .then(res => {
-            const data = res.data
-            obj.lat = data[0]?.lat
-            obj.lon = data[0]?.lon
-            obj.city = data[0]?.name
-            const country = data[0]?.country
-            const countryName = country === undefined ? undefined : Country.getCountryName(country)
-            obj.countryName = countryName
-        })
-        .catch((err) => { 
-            console.error(err)
-        })
-    return obj
+                .then(res => {
+                    const data = res.data
+                    const country = data[0].country
+                    const countryName = country === undefined ? 'undefined' : Country.getCountryName(country)
+                    obj.lat = data[0].lat,
+                    obj.lon = data[0].lon,
+                    obj.name = data[0].name,
+                    obj.countryName = countryName
+                })
+                .catch((err) => { 
+                    console.error(err)
+                })
+
+    return obj;
+    
 }
 
 export const getCountryForecast = async (lat:string, lon:string, key?:string) => {
-    let KEY = key === undefined ? ENV_API_KEY : key
+    let KEY = key !== undefined && key !== '' ? key : ENV_API_KEY
     const url = `${API_URL}${WEAHTER_URL}${WEATHER_PARAM.replace('?lat', lat).replace('?lon', lon).replace('?apikey', KEY)}`
     return await axios.get(url)
             .then(res => {
