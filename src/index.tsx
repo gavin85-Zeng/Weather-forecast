ee5e7b33abc66593db35b4cd4b0286c9ce876997
Gavin Zeng <gavin8503@gmail.com>
2022-06-05 21:12:32 +0800

    Add StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import './index.scss'
+import { store } from "./store";
 
-render(<App />, document.querySelector('#root'))
+render(
+    <React.StrictMode>
+        <Provider store={store}> 
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.querySelector('#root')
+)
